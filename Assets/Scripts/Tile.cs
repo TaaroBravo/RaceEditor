380e8d9be76f14231bbcb2656f6f6ce61f338460
Taaro Bravo <lautabdls@gmail.com>
2017-10-13 15:58:43 -0300

    Tiles.cs Actualizado
    TilesManager rompe la RAM?

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -3,6 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+<<<<<<< HEAD
+public class Tile : MonoBehaviour
+{
+    //Agregar elevaciones?
+    //Como hacer un enroque entre tiles?
+    public float gridScale = 2;
+    public float yPos = 0;
+    public float _xBound;
+    public float _zBound;
+
+    public Vector3 myPos;
+    public List<GameObject> neighborhoods = new List<GameObject>();
+
+    void Update()
+    {
+        if (Application.isEditor)
+        {
+            if (neighborhoods == null)
+                CalculateNeighborhoods();
+            if (myPos != transform.position)
+            {
+                CalculateCollision();
+                neighborhoods.Clear();
+                CalculateNeighborhoods();
+            }
+            myPos = transform.position;
+            if (_xBound == 0)
+                CalculateBounds();
+            Vector3 currentPosition = transform.position;
+            float xDifference = currentPosition.x % gridScale;
+            float zDifference = currentPosition.z % gridScale;
+            transform.position = new Vector3(currentPosition.x - xDifference, yPos, currentPosition.z - zDifference);
+        }
+    }
+
+    void CalculateBounds()
+    {
+        _xBound = GetComponent<Collider>().bounds.extents.x;
+        _zBound = GetComponent<Collider>().bounds.extents.z;
+    }
+
+    void CalculateNeighborhoods()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward * (_xBound + 0.5f), out hit))
+            neighborhoods.Add(hit.collider.gameObject);
+        if (Physics.Raycast(transform.position, -transform.forward * (_xBound + 0.5f), out hit))
+            neighborhoods.Add(hit.collider.gameObject);
+        if (Physics.Raycast(transform.position, transform.right * (_zBound + 0.5f), out hit))
+            neighborhoods.Add(hit.collider.gameObject);
+        if (Physics.Raycast(transform.position, -transform.right * (_zBound + 0.5f), out hit))
+            neighborhoods.Add(hit.collider.gameObject);
+    }
+
+    void CalculateCollision()
+    {
+        //Como hacer un enroque entre tiles?
+
+        foreach (var obj in Physics.OverlapBox(transform.position, GetComponent<Collider>().bounds.extents))
+        {
+            if (obj.transform.gameObject.tag == "Tile" && obj.transform.gameObject != transform.gameObject)
+            {
+                obj.transform.position = myPos;
+                //myPos = transform.position;
+            }
+        }
+
+        //List<bool> boolList = new List<bool>();
+        //boolList.Add(Physics.Raycast(transform.position, transform.forward, _xBound + 0.5f));
+        //boolList.Add(Physics.Raycast(transform.position, -transform.forward, _xBound + 0.5f));
+        //boolList.Add(Physics.Raycast(transform.position, transform.right, _zBound + 0.5f));
+        //boolList.Add(Physics.Raycast(transform.position, -transform.right, _zBound + 0.5f));
+        //foreach (var _bool in boolList)
+        //{
+        //    if(!_bool)
+        //    {
+        //        transform.position = new Vector3(6, 6, 6);
+        //    }
+        //}
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * (_xBound + 0.5f));
+        Gizmos.DrawLine(transform.position, transform.position - transform.forward * (_xBound + 0.5f));
+        Gizmos.DrawLine(transform.position, transform.position + transform.right * (_zBound + 0.5f));
+        Gizmos.DrawLine(transform.position, transform.position - transform.right * (_zBound + 0.5f));
+        //Gizmos.DrawCube(transform.position, GetComponent<Collider>().bounds.extents * 2);
+=======
 public class Tile : MonoBehaviour
 {
     //Agregar elevaciones?
@@ -94,5 +184,6 @@ public class Tile : MonoBehaviour
         Gizmos.DrawLine(transform.position, transform.position + transform.right * (_zBound + 0.5f));
         Gizmos.DrawLine(transform.position, transform.position -transform.right * (_zBound + 0.5f));
         //Gizmos.DrawCube(transform.position, GetComponent<Collider>().bounds.extents * 2);
+>>>>>>> 373f5715329f33f1f8253a45fda381383c51b33d
     }
 }
--- Assets/Scripts/TilesManager.cs
+++ Assets/Scripts/TilesManager.cs
@@ -1,3 +1,38 @@
+<<<<<<< HEAD
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//[ExecuteInEditMode]
+//public class TilesManager : MonoBehaviour {
+
+//    public List<GameObject> allTiles = new List<GameObject>();
+	
+//	void Update () {
+
+//        SearchingAllTiles();
+        
+         
+//	}
+
+//    void SearchingAllTiles()
+//    {
+//        foreach (var tile in GameObject.FindGameObjectsWithTag("Tile"))
+//        {
+//            if (allTiles.Count != 0)
+//            {
+//                for (int i = 0; i < allTiles.Count; i++)
+//                {
+//                    if (allTiles[i] != tile)
+//                        allTiles.Add(tile);
+//                }
+//            }
+//            else
+//                allTiles.Add(tile);
+//        }
+//    }
+//}
+=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -31,3 +66,4 @@ public class TilesManager : MonoBehaviour {
         }
     }
 }
+>>>>>>> 373f5715329f33f1f8253a45fda381383c51b33d
